2419d408c5210f5acf457806fc5b90c9ed696368
Camilo <ka_hilo13@hotmail.com>
2023-01-30 11:45:03 -0500

    Added migration

---
+++ database/migrations/2023_01_30_162442_pages.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('pages')) {
+            Schema::create('pages', function (Blueprint $table)
+            {
+                $table->increments('id');
+                $table->string('tittle');
+                $table->string('description');
+                $table->longText('image');
+                $table->integer('state')->default(1);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
